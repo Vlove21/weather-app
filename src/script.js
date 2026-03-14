652eaaf2f32983a291505d6b42ebabbda74f3447
Vlove21 <153148892+Vlove21@users.noreply.github.com>
2026-03-14 19:20:35 -0400

    Added script for Api response

--- src/script.js
+++ src/script.js
@@ -1,12 +1,21 @@
 function searchCity(city) {
   apiKey = "4tba4782084a6foabae1d06b62316bcd";
   apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  // let cityElement = document.querySelector("#city");
-  // cityElement.innerHTML = city;
+
   axios.get(apiUrl).then(refreshInfo);
 }
 function refreshInfo(response) {
-  console.log(response.data);
+  let cityName = response.data.city;
+  let temp = response.data.temperature.current;
+  let farenTemp = Math.round((temp * 9) / 5 + 32);
+  let country = response.country;
+  let windSpeed = Math.round(response.wind.speed);
+  let humidity = Math.round(response.temperature.humidity);
+  let feelsLike = response.temperature.feels_like;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = cityName;
+  let tempElement = document.querySelector("#city-temp");
+  tempElement.innerHTML = farenTemp;
 }
 
 function inputCity(event) {
