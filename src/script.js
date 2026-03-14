fdd94180fa0689ede9d13c6483107050377a6985
Vlove21 <153148892+Vlove21@users.noreply.github.com>
2026-03-14 19:51:42 -0400

    Completed refresh function

--- src/script.js
+++ src/script.js
@@ -5,16 +5,30 @@ function searchCity(city) {
   axios.get(apiUrl).then(refreshInfo);
 }
 function refreshInfo(response) {
+  console.log(response.data);
   let cityName = response.data.city;
+  let countryName = response.data.country;
   let temp = response.data.temperature.current;
   let farenTemp = Math.round((temp * 9) / 5 + 32);
-  //let country = response.data.country;
-  //let humidity = Math.round(response.data.temperature.humidity);
-  // let feelsLike = response.data.temperature.feels_like;
+  let humidity = Math.round(response.data.temperature.humidity);
+  let feelsLike = Math.round(response.data.temperature.feels_like);
+  let windSpeed = Math.round(response.data.wind.speed);
+  //Change the city, country, and Temp
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = cityName;
   let tempElement = document.querySelector("#city-temp");
   tempElement.innerHTML = farenTemp;
+  let countryElement = document.querySelector("#country");
+  countryElement.innerHTML = countryName;
+
+  //Changes the other infomartion
+  let humidElement = document.querySelector("#humid");
+  humidElement.innerHTML = humidity;
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = windSpeed;
+  let feelsLikeElement = document.querySelector("#actual-temp");
+  feelsLikeElement.innerHTML = feelsLike;
+  let iconElement = document.querySelector("weather icon");
 }
 
 function inputCity(event) {
