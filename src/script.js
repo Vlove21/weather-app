0f3fb9e14454c3bd2b3f2e6acdd5a861191016f5
Vlove21 <153148892+Vlove21@users.noreply.github.com>
2026-03-14 17:50:10 -0400

    added script to the form and added more elements

--- src/script.js
+++ src/script.js
@@ -1,9 +1,10 @@
-function searchCity(event){
-    event.preventDefault();
-
-
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#searched-city");
+  let cityElement = document.querySelector("#city");
+  let city = searchInput.value;
+  cityElement.innerHTML = city;
 }
 
-
 let SearchFormInput = document.querySelector("#search-bar");
-SearchFormInput.addEventListener("submit", searchCity)
+SearchFormInput.addEventListener("submit", searchCity);
