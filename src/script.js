836428f88669108c33dd816b6cc448eccc72338c
Vlove21 <153148892+Vlove21@users.noreply.github.com>
2026-03-14 19:08:47 -0400

    updated script

--- src/script.js
+++ src/script.js
@@ -1,23 +1,21 @@
 function searchCity(city) {
   apiKey = "4tba4782084a6foabae1d06b62316bcd";
   apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = city;
+  // let cityElement = document.querySelector("#city");
+  // cityElement.innerHTML = city;
   axios.get(apiUrl).then(refreshInfo);
 }
 function refreshInfo(response) {
-  console.log(response);
-  cityName = response.data;
+  console.log(response.data);
 }
 
 function inputCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#searched-city");
-
   let city = searchInput.value;
 
   searchCity(city);
 }
 
 let SearchFormInput = document.querySelector("#search-bar");
-SearchFormInput.addEventListener("submit", searchCity);
+SearchFormInput.addEventListener("submit", inputCity);
