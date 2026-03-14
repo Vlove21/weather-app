abcbe1e0d6117c10a27d9257772974ad77c5c688
Vlove21 <153148892+Vlove21@users.noreply.github.com>
2026-03-14 17:12:20 -0400

    Added javasciprt file

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function searchCity(event){
+    event.preventDefault();
+
+
+}
+
+
+let SearchFormInput = document.querySelector("#search-bar");
+SearchFormInput.addEventListener("submit", searchCity)
