24aeca240d0416c1a024cbf222bdb0be31058e3b
Vlove21 <153148892+Vlove21@users.noreply.github.com>
2026-03-16 15:51:33 -0400

    Added picture element and fixed the look of the app

--- src/script.js
+++ src/script.js
@@ -15,8 +15,9 @@ function refreshInfo(response) {
     (response.data.temperature.feels_like * 9) / 5 + 32,
   );
   let windSpeed = Math.round(response.data.wind.speed);
-  let date = new Date(response.data.time * 1000);
+  let date = response.data.time * 1000;
   let icon = `<img  src="${response.data.condition.icon_url}">`;
+  let conditions = response.data.condition.description;
 
   //Selects the elements
   let cityElement = document.querySelector("#city");
@@ -39,12 +40,54 @@ function refreshInfo(response) {
   iconElement.innerHTML = icon;
 
   getForecast(cityName);
+  getConditions(conditions, cityName);
 }
 
+function getConditions(conditions, city) {
+  console.log(conditions);
+  let conditionPics = document.querySelector("#conditions-pics");
+  let rainy = `<img src = "https://i0.wp.com/www.longbeachlocalnews.com/wp-content/uploads/IMG_5750.jpeg?fit=690%2C388&ssl=1" class= "conditions-pic" />`;
+  let windy = `<img src = "https://wach.com/resources/media2/16x9/1280/986/0x66/90/a156e677-9c7b-45f0-8229-920ad987af6f-WindyPalms.jpg" class= "conditions-pic" />`;
+  let sunny = `<img src = "https://blog.joffeemergencyservices.com/hubfs/maxresdefault.jpg" class= "conditions-pic" />`;
+  let storm = `<img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Lightning_Pritzerbe_01_%28MK%29.jpg/960px-Lightning_Pritzerbe_01_%28MK%29.jpg" class= "conditions-pic" />`;
+  let cloudy = `<img src = "https://images2.minutemediacdn.com/image/upload/c_fill,w_2160,ar_16:9,f_auto,q_auto,g_auto/shape%2Fcover%2Fsport%2FiStock-104472907-ec1d53a7c5724086414f13ae0dab8e1b.jpg" class="conditions-pic"/>`;
+  let snow = `<img src = "https://www.skisoutheast.com/wp-content/uploads/2016/11/2016-november-03-how-snowy.jpg" class= "conditions-pic" />`;
+  let conditionElement = document.querySelector("#current-conditions");
+  conditionElement.innerHTML = `There is currently ${conditions} in ${city}`;
+
+  if (conditions.includes("rain")) {
+    conditionPics.innerHTML = rainy;
+  }
+  if (conditions.includes("mist")) {
+    conditionPics.innerHTML = rainy;
+  }
+  if (conditions.includes("wind")) {
+    conditionPics.innerHTML = windy;
+  }
+  if (conditions.includes("clear")) {
+    conditionPics.innerHTML = sunny;
+  }
+  if (conditions.includes("sunny")) {
+    conditionPics.innerHTML = sunny;
+  }
+  if (conditions.includes("clouds")) {
+    conditionPics.innerHTML = cloudy;
+  }
+  if (conditions.includes("storm")) {
+    conditionPics.innerHTML = storm;
+  }
+  if (conditions.includes("snow")) {
+    conditionPics.innerHTML = snow;
+  }
+}
 //Converts time into date
 function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
+  let newDate = new Date(date);
+  let minutes = newDate.getMinutes();
+  let hours = newDate.getHours();
+  let ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
   let dates = [
     "Sunday",
     "Monday",
@@ -54,13 +97,13 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let day = dates[date.getDay()];
+  let day = dates[newDate.getDay()];
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
-  return `${day} ${hours}:${minutes}`;
+  return `${day} ${hours}:${minutes} ${ampm}`;
 }
 
 function formatDay(timestamp) {
